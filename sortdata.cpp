40f18db2fc43ad99302d9d0bd0ba88d03a5c15a3
Sivrong <sivrong3@gmail.com>
2022-08-17 13:35:31 +0700

    sort data

---
+++ sortdata.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class sortData
+{
+private:
+    int *data;
+    int dataSize, maxNum;
+
+public:
+    void randomize()
+    { // Constructor with parameters.
+        for (int i = 0; i < dataSize; i++)
+            data[i] = rand() % maxNum; // Error.
+    }
+
+    sortData(int size, string dataset, int max)
+    { // Constructor with parameters.
+        dataSize = size;
+        maxNum = max;
+        data = new int[dataSize];
+        if (dataset == "inorder")
+            // Generate in-order data starting from 0.
+            for (int i = 0; i < dataSize; i++)
+                data[i] = i;
+        else if (dataset == "reverseorder")
+            for (int i = 0; i < dataSize; i++)
+                data[i] = maxNum - i - 1;
+        else if (dataset == "random")
+            randomize();
+        else
+            // Error.
+            cout << "Error";    
+
+    }
+    void printData()
+    {
+        for (int i = 0; i < dataSize; i++)
+            cout << data[i] << endl;
+    }
+};
+
+int main()
+{
+    int dataSize = 100;
+    int maxNum = 200;
+
+    cout << "In-order data set." << endl;
+    sortData inorder(dataSize, "inorder", maxNum);
+    inorder.printData();
+    cout << endl << endl << endl;
+
+     cout << "Reverse-order data set." << endl;
+    sortData reverse(dataSize, "reverseorder", maxNum);
+    reverse.printData();
+    cout << endl << endl << endl << endl;
+
+    cout << "Random order data set." << endl;
+    sortData random(dataSize, "random", maxNum);
+    random.printData();
+    cout << endl;
+
+    return 0;
+}
